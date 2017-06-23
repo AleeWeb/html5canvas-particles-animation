fb49e0c7e3ac061008c8bc14132aee9b2829c76e
AleeWeb <aalexanderpr@gmail.com>
2017-06-22 22:00:52 -0400

    Lava Code Effect
    Set else if rule with minimum down to 2 radius to prevent it from
    completely disappearing.

--- particles.js
+++ particles.js
@@ -48,8 +48,10 @@ function Circle(x, y, dx, dy, radius) {
         //interactivity occurs
       if (mouse.x - this.x < 50 && mouse.x - this.x > -50
         && mouse.y - this.y < 50 && mouse.y - this.y > -50
-      ) else {
-        this.radius =+ 1;
+      ) {
+        this.radius += 1;
+      } else if(this.radius > 2){
+        this.radius -= 1;
         }
 
 			this.draw();
